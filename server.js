f55a6001727f9089982ae276f47347531ff51924
mohamed2g <70886237+mohamed2g@users.noreply.github.com>
2020-11-11 11:16:33 +0100

    changed a few things

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const server = express();
+const pg = require('pg');
 
 server.use(bodyParser.json());
 
 server.use(express.static('public'));
+
 
 server.set('port', (process.env.PORT || 8080));
 server.listen(server.get('port'), function() {
